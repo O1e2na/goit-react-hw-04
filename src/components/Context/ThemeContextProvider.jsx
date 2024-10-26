4af25c2420690564d85e497925603004eed853eb
O1e2na <155379499+O1e2na@users.noreply.github.com>
2024-10-26 14:04:18 +0300

    commit message

--- src/components/Context/ThemeContextProvider.jsx
+++ src/components/Context/ThemeContextProvider.jsx
@@ -1,30 +1,28 @@
-
-import { createContext, useEffect, useState } from "react"
-
-export const ThemeContext = createContext()
+import { useEffect, useState } from "react";
+import { ThemeContext } from "./ThemeContext";
 
 export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    const parsedValue = JSON.parse(localStorage.getItem('theme'));
+    return parsedValue ?? "blue";
+  });
 
-    const [theme, setTheme] = useState(() => {
-        const parsedValue = JSON.parse(localStorage.getItem('theme'))
-        return parsedValue ?? "blue"
-    })
+  const onSelect = (selectValue) => {
+    setTheme(selectValue);
+  };
 
-    const onSelect = (selectValue) => {
-        setTheme(selectValue)
-    }
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
 
-    useEffect(() => {
-        localStorage.setItem('theme', JSON.stringify(theme))
-    }, [theme])
-
-    return (
-        <ThemeContext.Provider
-            value={{
-                theme,
-                onSelect,
-            }}>
-            {children}
-        </ThemeContext.Provider>
-    )
-}
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        onSelect,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
